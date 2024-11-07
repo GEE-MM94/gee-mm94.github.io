99873187fb982e8958e3d921ef9ec697e0cbb1b3
MfundoM <mfundomkhasibe@gmail.com>
2024-11-07 11:27:50 +0200

    Complete site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 const navbar = document.querySelector(".navbar");
+const backToTopButton = document.getElementById("back-to-top");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.onclick = function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
